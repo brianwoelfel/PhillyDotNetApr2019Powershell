b15ff19d61cad5c046f31fb5f587848ab206750e
Brian Woelfel <brian_woelfel@comcast.com>
2019-04-01 15:54:03 -0400

    updates

--- 40_AllFilesInFolder.ps1
+++ 40_AllFilesInFolder.ps1
@@ -1,7 +1,6 @@
-#
+cls; Write-Host "### FIND ALL FILES IN A FOLDER ####"
 # Get a list of fully qualified paths like *.txt greater than 10 bytes
-# Get-ChildItem can get more than just folders.
-#
+# (also Get-ChildItem can get more than just folders)
 $FileObjArray = Get-ChildItem -Path c:\temp -Filter *.txt -Recurse
 foreach($FileObj in $FileObjArray) {
 	if($FileObj.Length -gt 10) {
