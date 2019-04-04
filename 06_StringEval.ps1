191aa0afc7d8d472e15239e251b349a0dc465ac1
Brian Woelfel <brianwoelfel@gmail.com>
2019-04-04 07:57:58 -0400

    fixes

--- 06_StringEval.ps1
+++ 06_StringEval.ps1
@@ -4,11 +4,22 @@ $B = "World"
 $C = 10
 
 Write-Host 'Test 1: $A $B $C' # Single quotes don't expand variables
+
 Write-Host "Test 2: $A $B $C" # Double quotes support variable expansion
+# C# version: Console.WriteLine($"Test 2: {a} {b} {c}");
+
 Write-Host "Test 3: $A $B $($C + 5)" # Within double quotes use $() to evaluate code
+# C# version: Console.WriteLine($"Test 3: {a} {b} {(c + 5)}");
+
 # Start multiline string with @" and end "@ on it's own line
 Write-Host @"
-Test 4: multiline string with evaluation,
-   and use backtick to escape dollar sign
-`$A=$A `$B=$B
+    Test 4: multiline string with evaluation,
+    and use backtick to escape dollar sign
+    `$A=$A `$B=$B
 "@
+
+# C# version: Console.WriteLine($@"
+#    Test 4: multiline string with evaluation,
+#    a={a} b={b}
+# ");
+  
---
+++ cs/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringEval();
+        }
+
+        static void StringEval()
+        {
+            Console.WriteLine("#### STRING EVALUATION ####");
+            string a = "Hello"; // Define some variables
+            string b = "World";
+            int c = 10;
+
+            //Console.WriteLine('Test 1: $A $B $C'); // Single quotes don't expand variables
+            Console.WriteLine($"Test 2: {a} {b} {c}"); // Double quotes support variable expansion
+            Console.WriteLine($"Test 3: {a} {b} {(c + 5)}"); // Within double quotes use $() to evaluate code
+            // Start multiline string with @" and end "@ on it's own line
+            Console.WriteLine($@"
+            Test 4: multiline string with evaluation,
+               and use backtick to escape dollar sign
+            a={a} b={b}
+            ");
+        }
+
+        public static void Array()
+        {
+            string[] myArray = { "a", "b", "c" };
+            
+        }
+
+        public static void Hashtable()
+        {
+            Hashtable<string, string> hash = { { "a", "1" }, { "b", "2" } };
+        }
+    }
+}
