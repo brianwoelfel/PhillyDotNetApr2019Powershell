82954ed4cd1032bcca0d50c1b29336d42e8988d4
Brian Woelfel <brian_woelfel@comcast.com>
2019-04-04 08:03:38 -0400

    updates

---
+++ MyModule/ArrayUtil.ps1
@@ -0,0 +1,55 @@
+function Invoke-HelloWorld {
+	Write-Host "Hello world"
+}
+
+function Convert-HashtableToString {
+<#
+.SYNOPSIS
+	Convert hashtable to string like key1=value2, key2=value2, ...
+
+.EXAMPLE
+	Convert-HashtableToString ${ "A"="1"; "B"="2"; "C"="3"; } 
+	Returns "A=1,B=2,C=3"
+#>
+	[cmdletBinding()]
+	param (
+		[Parameter(ValueFromPipeline=$True)]
+		$Hash
+	)
+
+	return ( $Hash.Keys | sort | % { "$($_)=$($Hash[$_])" } ) -join ', '
+}
+
+function Remove-ItemFromArray($Item,[array]$Array)
+{
+<#
+.SYNOPSIS
+	Return an array with all occurrences of specified item removed
+
+.EXAMPLE
+	Remove-ItemFromArray -Item "A" -Array @("A", "B", "C", "A", "D")
+	Returns @("B", "C", "D")
+	
+.NOTES
+	Brian Woelfel 7/1/2018
+#>
+	return $Array | ? { $_ -ne $Item }
+}
+
+function Get-ArraySubtraction([array]$SomeList, [array]$removeThisStuff) {
+<#
+.SYNOPSIS
+	Return array $SomeList with all matching entries from array $removeThisStuff removed.
+
+.EXAMPLE
+	Get-ArraySubtraction @("A", "B", "C") @("C")
+	Returns @("A", "B") 
+	
+.NOTES
+	Brian Woelfel 7/1/2018
+#>
+	$removeThisStuff | % { 
+		$SomeList = Remove-ItemFromArray -Item $_ -Array $SomeList
+	}
+	return $SomeList
+}
---
+++ MyModule/MyModule.psm1
@@ -0,0 +1,34 @@
+﻿# Explicitly list all the powershell files we're including in this module
+. $PSScriptRoot\ArrayUtil.ps1
+
+# Explicitly list all functions we're exporting
+Export-ModuleMember -Function 'Convert-CsvStringToExcel'
+
+$NugetPackagesToLoad = @("NPoi", "EPPlus")
+
+#
+# Load Nuget packages to support reading Excel files
+#
+# Explanation of this technique to load nuget packages into powershell:
+# https://stackoverflow.com/questions/39257572/loading-assemblies-from-nuget-packages
+# and 
+# https://social.technet.microsoft.com/Forums/en-US/8248d617-816b-4435-924d-5dab7de2774d/try-catch-what-is-the-exception-type?forum=winserverpowershell
+#
+$NugetPackagesToLoad | % {
+	[string]$PackageName = $_
+	# Make sure package is installed
+	try {
+		Get-Package $PackageName -ErrorAction Stop | Out-Null
+	} catch {
+		throw "Nuget Package $PackageName not installed"
+	}
+
+	# Load package into the current session
+	try {
+		(Get-ChildItem -Filter *.dll -Recurse (Split-Path (get-package $PackageName).Source)).FullName | % { 
+			[Reflection.Assembly]::LoadFrom($_) | Out-Null 
+		}
+	} catch {
+		throw "Failed attempting to load $PackageName toolkit $_"
+	}
+}
