e0d8e4907103efc99d83c99c229139d48f40d940
Brian Woelfel <brianwoelfel@gmail.com>
2019-04-12 07:49:03 -0400

    fixes

--- 70_MoreStuff.ps1
+++ 70_MoreStuff.ps1
@@ -14,12 +14,16 @@ Didn't fit into this demo, but you should know:
 * PESTER is an excellent unit testing kit for powershell.  Follows the same pattern as JUnit, NUnit, Mocha, etc.
   Syntax is a little annoying but if you're doing anything nontrivial test-driven-development is a great asset.
 
-* try/catch/finally works in Powershell
+* try/catch/finally works in Powershell, but the syntax is a little weirder than C#
 
 * $Input is a reserved word
+
+* [switch] syntax lets you allow for optional boolean parameters without the caller explicitly supplying $true
 
 * PowerShellCore is rewritten using DotNet core and is platform independent.  Syntax is the same, but it's not
   always clear how third party dependencies work.
 
 * Lots of stuff in powershell about formatting output as tables, but the syntax is weird and hard to use.
+
+* Learn how to use "strict mode" to catch syntax errors early
 #>
--- 99_End.ps1
+++ 99_End.ps1
@@ -6,6 +6,7 @@ Final thoughts:
   but be nice to your future replacements and use the long form when writing code 
 * A real devops or sysadmin will appreciate you using proper function naming syntax,
   proper commenting with examples, and unit tests.
+* Unlike real programming languages, you really can fake Powershell in a few days
 
 Materials:
 https://github.com/brianwoelfel/PhillyDotNetApr2019Powershell   
@@ -19,5 +20,6 @@ http://linkedin.com/in/brianwoelfel
 Eigen X specializes in Salesforce, Tableau, PowerBI, .NET/Java/NodeJS software dev, Agile/PM/PMO.
 
 If you're working in Philly reach out to me and let's have lunch!
-I've got a lot to say about Angular, Jenkins, GitHub, Kubernetes, Docker, MongoDB, and AWS.
+I've got lots of opinions on DotNet Core, Angular, Jenkins, GitHub, Kubernetes, Docker, MongoDB, and AWS
+including "I don't understand what's going on" and "How does this work?""
 #>
