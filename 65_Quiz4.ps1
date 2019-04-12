0052c08fb3370690c8be1ed71a4019bf309f3b90
Brian Woelfel <brianwoelfel@gmail.com>
2019-04-12 16:31:50 -0400

    fixes

--- 65_Quiz4.ps1
+++ 65_Quiz4.ps1
@@ -1,18 +1,5 @@
 cls; Write-Host "#### QUIZ 4 ####"
 
 # Quiz- what does this do?
-# If you read ahead, don't blurt out the answer please.
-( @(1..100) | ? { 
-	$N=$_; 
-	$P=$true; 
-	$D=[math]::Sqrt($N); 
-	2..$D | % { 
-		if($N % $_ -eq 0) { 
-			$P = $false 
-		}
-	}
-	if($P) { 
-		$D 
-	}
-} ) -join ','
+
 
